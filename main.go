77340106691d5aa90a3009bf4b0e59217651486a
morlako <ryanmcvicker12@gmail.com>
2020-11-16 01:08:45 -0500

    tried to remove all files from a broken html template

--- main.go
+++ main.go
@@ -4,20 +4,36 @@
 package main
 
 import (
-	"fmt"
-	"ioutil"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintf(w, "home page")
+	//pageTitle := "Ryan Portfolio Home Page"
+
+	pageBody, err := ioutil.ReadFile("index.html")
+
+	if err != nil { log.Fatal(err) }
+
+	t, _ := template.ParseFiles("index.html")
+
+	t.Execute(w, pageBody)
+
 }
 
 func main(){
 
-	http.HandleFunc("/", handler)
+	//Tells the webserver to create the endpoint "/" and display handler
+
+	http.HandleFunc("/",indexHandler)
+
+	// Wraps the ListenAndServe function to handle any errors
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+	//ListenAndServe() always returns an error
 }
 
