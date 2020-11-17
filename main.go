2188f9d91957699b8e0eb5791ec0f328767536e6
morlako <ryanmcvicker12@gmail.com>
2020-11-17 01:53:11 -0500

    added an html template wrom w33.css and made small changes to some of the fields

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 // Portfolio website for my projects written in Golang
-
+// Used free template for the html on "https://www.w3schools.com/w3css/w3css_templates.asp"
 
 package main
 
 import (
-	"html/template"
 	"io/ioutil"
 	"net/http"
 	"log"
+	"fmt"
 )
+
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
-	//pageTitle := "Ryan Portfolio Home Page"
-
-	pageBody, err := ioutil.ReadFile("index.html")
+	stream, err := ioutil.ReadFile("index.html")
 
 	if err != nil { log.Fatal(err) }
 
-	t, _ := template.ParseFiles("index.html")
+	indexFile := string(stream)
 
-	t.Execute(w, pageBody)
+	fmt.Fprintf(w, indexFile)
 
+	//should this be in the main func?
+
+	
+	
 }
 
 func main(){
@@ -29,7 +32,6 @@ func main(){
 	//Tells the webserver to create the endpoint "/" and display handler
 
 	http.HandleFunc("/",indexHandler)
-
 	// Wraps the ListenAndServe function to handle any errors
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
